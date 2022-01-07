fe5c51d9be99e55932c2ece8e40239b29a48ebb2
ccastrofymtechcom <ccastro@fymtech.com>
2022-01-07 12:39:46 -0500

    masEjercicios

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,50 @@
-let sumar = (numero1, numero2) => numero1 + numero2;
+/* let sumar = (numero1, numero2) => numero1 + numero2;
 let restar = (numero1, numero2) => numero1 - numero2;
 let multiplicar = (numero1, numero2) => numero1 * numero2;
 let dividir = (numero1, numero2) => numero1 / numero2;
 
 let calculadora = (numero1, numero2, operacion) => operacion(numero1, numero2);
 console.log(calculadora(5,8,sumar));
+ 
+
+function suma (numero1, numero2){
+    return numero1 + numero2;
+}
+function resta (numero1, numero2){
+    return numero1 - numero2;
+}
+function multiplicacion (numero1, numero2){
+    return numero1 * numero2;
+}
+function division (numero1, numero2){
+    return numero1 / numero2;
+}
+
+function calculadora (numero1, numero2, operacion){
+    return operacion(numero1, numero2);
+}
+
+console.log (calculadora (5,4,suma))
+
+ */
+
+/*****************************For  +  CALLBACK******************************************/
+
+function agregarHttp(url) {
+    return "http://" + url;
+}
+
+function contarURL(array){
+    return array.length;
+}
+
+function procesar(array,callback){
+  let urls = [];
+    for (let i=0; i< array.length; i++){
+        urls.push(callback(array[i]));
+    }
+  return urls;
+}
+
+console.log(procesar(["www.google.com","www.yahoo.com"],contarURL))
+console.log(procesar(["www.google.com","www.yahoo.com"],agregarHttp))
---
+++ foreach.js
@@ -0,0 +1,16 @@
+let listaDeSuperMercado = [
+    'Bife de chorizo', 
+    'Coca Cola', 
+    'Bananas', 
+    'Lechuga', 
+    'Chimichurri', 
+    'Lata de tomates', 
+    'Arvejas', 
+    'Cereales', 
+    'Pechuga de pollo', 
+    'Leche'
+];
+
+let listaDeSupermercado = listaDeSuperMercado.forEach(function(lista){
+    return console.log(lista);
+});

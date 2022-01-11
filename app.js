b2b53da770599b8e4b1ee39790fe1c2f2ac1e2c8
ccastrofymtechcom <ccastro@fymtech.com>
2022-01-10 23:20:40 -0500

    EjercicioAPP

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 
-let archivoTareas = JSON.parse(
-fs.readFileSync("tareas.json", 'utf-8')
-);
+var archivoTareas = JSON.parse(fs.readFileSync("tareas.json", 'utf-8'));
+var arrayTareas = [];
+var tareas = [];
 
-for (let i = 0; i < archivoTareas.length; i++){
-console.log(archivoTareas[i])
-}
+let estadoSolicitado = (estado) => {
+    let estadoMinusculas = estado.toLowerCase();
+    for (let i = 0; i < archivoTareas.length; i++){
+        arrayTareas=archivoTareas[i];
+        arrayTareas.estado == estadoMinusculas ? tareas.push(arrayTareas): tareas;
+    }
+    return tareas;  
+};
+
+console.log(estadoSolicitado('PenDientE'));

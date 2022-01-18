40a73ee3952205382c4bca80cdecb2fbb170324c
ccastrofymtechcom <ccastro@fymtech.com>
2022-01-18 09:00:57 -0500

    FinalCierreModulo

--- autosModule.js
+++ autosModule.js
@@ -7,7 +7,7 @@ let autos = [{
         cuotas :12, 
         anio :2019, 
         patente :'APL123', 
-        vendido: true
+        vendido: false
     },
     {
         marca : 'Toyota',
@@ -18,7 +18,18 @@ let autos = [{
         cuotas :14, 
         anio :2019, 
         patente :'JJK116', 
-        vendido: true
+        vendido: false
+    },
+    {
+        marca : 'Hyundai',
+        modelo :'Gravity', 
+        precio : 100000,
+        km :0, 
+        color :'Blanco', 
+        cuotas :14, 
+        anio :2019, 
+        patente :'JJK116', 
+        vendido: false
     }
 ];
 
